3ef649da6c6a1705eb93dc61d506fc12127d5673
Agustina <agustinafigueroacardozo@gmail.com>
2026-04-02 19:38:18 -0300

    Hola

--- OBLIGATORIO 1 P2/Aplicaciones de Consola/Program.cs
+++ OBLIGATORIO 1 P2/Aplicaciones de Consola/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Alo Hola Chao");
+            Console.WriteLine("Hello Aloha Hola Chao");
         }
     }
 }
