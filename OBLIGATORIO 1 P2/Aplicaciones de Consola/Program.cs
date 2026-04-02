a024b2bc4f5bc238178572bc955b3a5470d0c07a
Agustina <agustinafigueroacardozo@gmail.com>
2026-04-02 19:37:01 -0300

    Initial commit

---
+++ OBLIGATORIO 1 P2/Aplicaciones de Consola/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Aplicaciones_de_Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Alo Hola Chao");
+        }
+    }
+}
