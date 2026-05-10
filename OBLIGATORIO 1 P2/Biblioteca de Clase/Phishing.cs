9f207179d43c90f74bbdb2ef07863735bffb9cc1
Agustina <agustinafigueroacardozo@gmail.com>
2026-05-10 20:49:55 -0300

    IValidar en Phishing

--- OBLIGATORIO 1 P2/Biblioteca de Clase/Phishing.cs
+++ OBLIGATORIO 1 P2/Biblioteca de Clase/Phishing.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Biblioteca_de_Clase
 {
-    public class Phishing : Incidente
+    public class Phishing : Incidente, IValidar
     {
         #region GET and SET
         public string Canal { get; set; }
